be0b89cfb7c93d60281731269d957efcfe9cd54f
Jackson <jacksoncontreras4@gmail.com>
2021-04-12 16:36:23 -0700

    getopt

--- driver.cpp
+++ driver.cpp
@@ -3,18 +3,38 @@
 #include <stdlib.h> 
 #include <string.h>
 #include <iostream>
-#include <inttypes.h>
 #include <unistd.h>
 #include <vector>
 #include <math.h>    
 #include <pthread.h>   
 
 
-
-
-
-
 int main(int argc, char **argv) {
 printf("HELLO WORLD\n");
+int option;
+  int samples;
+  char argument[256];
+  char *o_value;
+  int *n_value;
+
+  int i = 0;
+  while ((option = getopt(argc, argv, "E:L:f:e")) != -1){
+      switch (option){
+        case 'E':
+            printf("f character found\n");
+            break;
+        case 'L':
+            printf("L Character found\n");
+            break;
+        case 'f':
+            printf("f Character found\n");
+            break;
+        case 'e':
+            printf("e character found\n");
+            break;
+
+      }
+    
+  }
 return 0;
 }
